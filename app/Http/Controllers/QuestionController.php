6cc4b76290139f00fa645283838622e286188e57
sukraalhamda <sukra24si@mahasiswa.pcr.ac.id>
2025-09-29 10:40:20 +0700

    Buat value pada nama,email,pertanyaan

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -27,9 +27,20 @@ class QuestionController
     public function store(Request $request)
     {
         //dd($request->all());
+
+        $request->validate([
+            'nama'       => 'required|max:10',
+            'email'      => ['required', 'email'],
+            'pertanyaan' => 'required|max:300|min:8',
+        ], [
+            'nama.required' => 'Nama tidak boleh kosong',
+            'email.email'   => 'Email Tidak valid',
+        ]);
+
         $data['nama']       = $request->nama;
         $data['email']      = $request->email;
         $data['pertanyaan'] = $request->pertanyaan;
+
         return view('home-question-respon', $data);
     }
 
