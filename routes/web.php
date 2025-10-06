d90cdf462011298e2f4c47a13116bfb61357dff3
sukraalhamda <sukra24si@mahasiswa.pcr.ac.id>
2025-10-06 09:53:53 +0700

    buat dashboard admin pertemuan 5

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\MahasiswaController;
 
 Route::get('/pegawai', [PegawaiController::class, 'index']);{
@@ -35,3 +36,5 @@ Route::get('/about', function () {
 
 Route::post('question/store', [QuestionController::class, 'store'])
 		->name('question.store');
+
+Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
